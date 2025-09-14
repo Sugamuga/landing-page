bf860758e5538f3250dd666116f5783139b41514
Mustafa Cor <codercor@Mac.sunrise.box>
2025-09-14 21:03:44 +0200

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   eslint: {
-    ignoreDuringBuilds: true, // ✅ Lint hataları build’i bozmasın
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Lint hataları build'i bozmasın
   },
+  output: 'standalone', // ✅ Docker standalone build için gerekli
 }
 
 export default nextConfig;
